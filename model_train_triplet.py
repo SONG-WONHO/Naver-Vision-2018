59e22c9bbef07ee5724242853f9b5fee0605f2eb
SONG-WONHO <win778500@gmail.com>
2019-01-31 00:57:59 +0900

    Update model_train_triplet.py

--- model_train_triplet.py
+++ model_train_triplet.py
@@ -18,6 +18,7 @@ from keras.models import Model
 from keras import backend as K
 
 import util
+import data_loader
 
 
 def bind_model(model):
@@ -38,75 +39,6 @@ def bind_model(model):
 
 
 if __name__ == '__main__':
-
-    def train_mac_loader(train_dataset_path, input_shape, model):
-
-        def train_data_loader(train_dataset_path, img_size):
-            # image list
-            img_list = []
-
-            # global variables
-            label_list = []
-            label_dic = {}
-            label_idx = 0
-            i = 0
-            # train data load
-            for root, dirs, files in os.walk(train_dataset_path):
-                if not files: continue
-                print(i)
-                label_dic['label_' + str(label_idx)] = [os.path.join(root, file_name) for file_name in files if files]
-
-                for filename in files:
-                    img_path = os.path.join(root, filename)
-
-                    try:
-                        img = cv2.imread(img_path, 1)
-                        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                        img = cv2.resize(img, img_size)
-                        i += 1
-                    except:
-                        continue
-
-                    img_list.append(img)
-                    label_list.append(label_idx)
-
-                label_idx += 1
-
-                if label_idx == 250: break
-
-            label_info = {
-                "label_list": label_list,
-                "label_dic": label_dic,
-                "label_idx": label_idx
-            }
-
-            return np.asarray(img_list), label_info
-
-        # load train set
-        img_list, label_info = train_data_loader(train_dataset_path, input_shape[:2])
-        print("이미지 로드 완료 ...")
-
-        # apply cnn filter
-        num = 10
-        get_feature_layer = K.function([model.layers[0].input] + [K.learning_phase()], [model.layers[-1].output])
-
-        idx = 1
-        img_vecs = np.empty((len(img_list), 7, 7, 512))
-        while idx * num <= len(img_list):
-            img_vecs[num * (idx - 1):num * idx] = get_feature_layer([img_list[num * (idx - 1):num * idx], 0])[0]
-            idx += 1
-
-        if num * (idx - 1) != len(img_list):
-            img_vecs[num * (idx - 1):] = get_feature_layer([img_list[num * (idx - 1):], 0])[0]
-
-        del img_list
-        gc.collect()
-        print("맥 로드 완료 ...")
-
-        # calculate mac feature
-        img_vecs = util.cal_mac(img_vecs)
-
-        return img_vecs, label_info
 
     def get_label_bound(label_info):
         label_bound = {}
@@ -140,6 +72,16 @@ if __name__ == '__main__':
 
         return Model(inputs=inp, outputs=out)
 
+    def get_validation_idx(data_size, val_size):
+        val_set_size = int(data_size * val_size)
+        np.random.seed(0)
+
+        shuffled_indices = np.random.permutation(data_size)
+        train_indices = shuffled_indices[val_set_size:]
+        val_indices = shuffled_indices[:val_set_size]
+
+        return train_indices, val_indices
+
     # load VGG16
     base_model = "vgg16"
     model = util.select_base_model(base_model)
@@ -156,11 +98,22 @@ if __name__ == '__main__':
     # parameter
     input_shape = (224, 224, 3)  # input image shape
 
+    output_path = ['mac_list_v1.pkl', 'label_info_v1.pkl']
+
     """ Load data """
     print('dataset path', DATASET_PATH)
     train_dataset_path = DATASET_PATH + '/train/train_data'
 
-    img_vecs, label_info = train_mac_loader(train_dataset_path, input_shape, model)
+    nsml.cache(data_loader.train_data_mac_loader,
+               data_path=train_dataset_path,
+               img_size=input_shape,
+               output_path=output_path,
+               model=model)
+
+    with open(output_path[0], 'rb') as img_f:
+        img_vecs = pickle.load(img_f)
+    with open(output_path[1], 'rb') as label_f:
+        label_info = pickle.load(label_f)
 
     # l2 norm
     img_vecs = img_vecs / np.linalg.norm(img_vecs, axis=1).reshape(-1, 1)
@@ -196,9 +149,21 @@ if __name__ == '__main__':
 
     triplet_images = np.array(triplet_images)
 
+    # validation split
+    train_idx, val_idx = get_validation_idx(len(triplet_images), 0.1)
+
+    triplet_images_val = triplet_images[val_idx]
+    triplet_images = triplet_images[train_idx]
+
+    # train anchor, positive, negative
     anchor_images = triplet_images[:, 0]
     positive_images = triplet_images[:, 1]
     negative_images = triplet_images[:, 2]
+
+    # validation anchor, positive, negative
+    anchor_images_val = triplet_images_val[:, 0]
+    positive_images_val = triplet_images_val[:, 1]
+    negative_images_val = triplet_images_val[:, 2]
 
     # triplet model
 
@@ -225,21 +190,39 @@ if __name__ == '__main__':
     model = Model(inputs=[anchor_item_input, positive_item_input, negative_item_input], outputs=loss)
     model.summary()
 
-    # hyper parameter
-    batch_size = 64
-    epochs = 100
-    data_cnt = len(triplet_images)
+    bind_model(model)
 
-    print(data_cnt)
+    # hyper parameter
+    batch_size = 128
+    epochs = 1000
+    train_size = len(triplet_images)
+    val_size = len(triplet_images_val)
+
+    print(train_size, val_size)
 
     # optimizer
     opt = keras.optimizers.Adam(lr=0.0001, decay=0, amsgrad=False)
     model.compile(optimizer=opt, loss="mean_squared_error")
 
-    hist = model.fit(x=[anchor_images, positive_images, negative_images],
-                     y=[0] * data_cnt,
-                     epochs=epochs,
-                     batch_size=batch_size)
+    """ Training loop """
+    for epoch in range(epochs):
+        res = model.fit(x=[anchor_images, positive_images, negative_images],
+                        y=[0]*train_size,
+                        batch_size=batch_size,
+                        initial_epoch=epoch,
+                        epochs=epoch + 1,
+                        verbose=1,
+                        shuffle=True,
+                        validation_data=([anchor_images_val, positive_images_val, negative_images_val], [0]*val_size))
+
+        print(res.history)
+        train_loss = res.history['loss'][0]
+        val_loss = res.history['val_loss'][0]
+        nsml.report(summary=True, step=epoch, epoch=epoch, epoch_total=epochs, loss=train_loss, val_loss=val_loss)
+        if epoch % 5 == 0:
+            nsml.save(epoch)
+
+    nsml.save('saved!')
 
     print("done!")
     exit()
